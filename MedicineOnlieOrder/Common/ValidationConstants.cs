7bafba05576e6bcce1c2e59b910bd5639101900e
Daniel <danielpaskalev14@Gmail.com>
2026-01-18 16:35:45 +0200

    Create "Validation COnstants" class

---
+++ MedicineOnlieOrder/Common/ValidationConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace MedicineOnlieOrder.Common
+{
+    public static class ValidationConstants
+    {
+        public const byte MedicineNameMinLenght = 3;
+        public const byte MedicineNameMaxLenght = 250;
+        public const double MedicinePriceMinValue = 0;
+        public const double MedicinePriceMaxValue = 10000;
+        public const byte MedicineDescriptionMinValue = 0;
+        public const int MedicineDescriptionMaxValue = 10000;
+
+        public const byte MedicineTypeNameMinLenght = 0;
+        public const byte MedicineTypeNameMaxLenght = 250;
+    }
+}
