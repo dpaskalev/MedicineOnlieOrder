95027119eec79209d149f93cb1937047deb053b9
Daniel <danielpaskalev14@Gmail.com>
2026-01-18 16:35:15 +0200

    Create "MedicineType" Class

---
+++ MedicineOnlieOrder/Data/DataModels/MedicineType.cs
@@ -0,0 +1,15 @@
+﻿using MedicineOnlieOrder.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicineOnlieOrder.Data.DataModels
+{
+    public class MedicineType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ValidationConstants.MedicineTypeNameMaxLenght)]
+        public string MedicineTypeName { get; set; } = null!;
+    }
+}
