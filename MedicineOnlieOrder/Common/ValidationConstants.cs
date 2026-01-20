e1cb82a8d4dab62dadb2bd4290148c89e6e5846a
Daniel <danielpaskalev14@Gmail.com>
2026-01-20 17:41:28 +0200

    Create "Pharmacy" data model

--- MedicineOnlieOrder/Common/ValidationConstants.cs
+++ MedicineOnlieOrder/Common/ValidationConstants.cs
@@ -11,5 +11,10 @@
 
         public const byte MedicineTypeNameMinLenght = 0;
         public const byte MedicineTypeNameMaxLenght = 250;
+
+        public const byte PharmacyNameMinlenght = 0;
+        public const int PharmacyNameMaxLebnght = 1000;
+        public const byte PharmacyLocationMinLenght = 0;
+        public const int PharmacyLocationMaxLenght = 1000;
     }
 }
