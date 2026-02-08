b75574b66ecc73ab60747d22801f1628ecc11bbe
Daniel <danielpaskalev14@Gmail.com>
2026-02-08 17:00:49 +0200

    Create "UserMedicine"

---
+++ DataModels/Data/DataModels/UserMedicine.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels.Data.DataModels
+{
+    [PrimaryKey(nameof(UserId), nameof(MedicineId))]
+    public class UserMedicine
+    {
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; } = null!;
+
+        [Required]
+        public int MedicineId { get; set; }
+
+        [ForeignKey(nameof(MedicineId))]
+        public Medicine Medicine { get; set; } = null!;
+    }
+}
