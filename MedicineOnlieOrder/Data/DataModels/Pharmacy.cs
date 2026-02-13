85d048a60241b6353db06a503fee06b58dc8d3a1
Daniel <danielpaskalev14@Gmail.com>
2026-02-13 21:18:55 +0200

    add collection to "Pharmacy" dataModel

--- MedicineOnlieOrder/Data/DataModels/Pharmacy.cs
+++ MedicineOnlieOrder/Data/DataModels/Pharmacy.cs
@@ -26,6 +26,6 @@ namespace MedicineOnlieOrder.Data.DataModels
 
         public bool IsDeleted { get; set; } = false;
 
-        //collection
+        public ICollection<PharmacyMedicine> PharmaciesMedicines { get; set; } = new List<PharmacyMedicine>();
     }
 }
