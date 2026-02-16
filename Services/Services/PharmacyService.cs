6b6f51fbbd7a6e724907cb3b56bc4a7d3eb47fde
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 18:22:24 +0200

    Add "PharmacyService"

---
+++ Services/Services/PharmacyService.cs
@@ -0,0 +1,39 @@
+﻿using DataModels.Data;
+using Microsoft.EntityFrameworkCore;
+using Services.Services.Interfaces;
+using Services.VewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class PharmacyService : IPharmacyService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PharmacyService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<PharmacyViewModel>> GetPharmaciesAsynk(string userId)
+        {
+            var modelsCollection = await _context.Pharmacies
+                .Where(p => p.IsDeleted == false)
+                .ToListAsync();
+
+            var pharmacyViewModels = modelsCollection.Select(p => new PharmacyViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Location = p.Loctaion,
+                IsPublisher = p.UserId == userId
+            });
+
+            return pharmacyViewModels;
+        }
+    }
+}
