1e0344e95cb72809e11816ad981d9a9d4d1c6f9b
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 16:57:56 +0200

    Create "MedicineViewModel"

---
+++ Services/VewModels/MedicineViewModel.cs
@@ -0,0 +1,34 @@
+﻿using DataModels.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.VewModels
+{
+    public class MedicineViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(ValidationConstants.MedicineNameMaxLenght, ErrorMessage = "Incorect name lenght")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Experation date is required")]
+        public DateTime ExperationDate { get; set; }
+
+        [Required(ErrorMessage = "Proce is required")]
+        [Range(ValidationConstants.MedicinePriceMinValue, ValidationConstants.MedicinePriceMaxValue)]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; } = null!;
+
+        public string? ImageURL { get; set; }
+
+        [Range(ValidationConstants.MedicineTypeMinCount, ValidationConstants.MedicineTypeMaxCount)]
+        public int Type { get; set; }
+
+        public IEnumerable<TypeVewModel>? MedicineTypes { get; set; }
+    }
+}
