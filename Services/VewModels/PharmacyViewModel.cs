7e1f681f5b6b5f0be8f4cc24bac61a3693d9cfdb
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 18:22:55 +0200

    Create "PharmacyViewModel"

---
+++ Services/VewModels/PharmacyViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DataModels.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.VewModels
+{
+    public class PharmacyViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Pharmacy name is reqired")]
+        [StringLength(ValidationConstants.PharmacyNameMaxLebnght, ErrorMessage = "Pharmacy name is too long")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Pharmacy name is reqired")]
+        [StringLength(ValidationConstants.PharmacyLocationMaxLenght, ErrorMessage = "Location name is too long")]
+        public string Location { get; set; } = null!;
+
+        [Required]
+        public bool IsPublisher { get; set; }
+    }
+}
