04ffb9d26afbc27074b8a94cd5c6ce5abb582071
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 18:39:36 +0200

    create "PharmacyDetails" & "PharmacyMedicine" view models

---
+++ Services/VewModels/PharmacyDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.VewModels
+{
+    public class PharmacyDetailsViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Location { get; set; } = null!;
+
+        public List<PharmacyMedicineViewModel> Medicines { get; set; } = new List<PharmacyMedicineViewModel>();
+    }
+}
---
+++ Services/VewModels/PharmacyMedicineViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.VewModels
+{
+    public class PharmacyMedicineViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int PharmacyId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public bool IsPublisher { get; set; }
+    }
+}
