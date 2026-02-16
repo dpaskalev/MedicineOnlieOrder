6f13285a6ca172cc3e6590cec4d71496607cf5ba
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 16:14:58 +0200

    Add "MEdicineController"

--- MedicineOnlieOrder/Controllers/MedicineController.cs
+++ MedicineOnlieOrder/Controllers/MedicineController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services.Interfaces;
 
 namespace MedicineOnlieOrder.Controllers
 {
+    [Authorize]
     public class MedicineController : Controller
     {
+        private readonly IMedicineService _medicineService;
+
+        public MedicineController(IMedicineService medicineService)
+        {
+            _medicineService = medicineService;
+        }
+
 
     }
 }
