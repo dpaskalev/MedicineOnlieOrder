4223d7d08e366d4f7146f1ffe8701a2728424a0e
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 13:24:39 +0200

    Create "MedicineService"

---
+++ Services/Services/MedicineService.cs
@@ -0,0 +1,22 @@
+﻿using DataModels.Data;
+using Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class MedicineService : IMedicineService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedicineService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
