cd11b95c6bd137e6e80e6de6141a49382090528e
Daniel <danielpaskalev14@Gmail.com>
2026-02-16 16:27:08 +0200

    Add "GetIndex" to MedicineService

--- Services/Services/MedicineService.cs
+++ Services/Services/MedicineService.cs
@@ -1,5 +1,7 @@
 ﻿using DataModels.Data;
+using MedicineOnlieOrder.VewModels;
 using Services.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,25 @@ namespace Services.Services
             _context = context;
         }
 
+        public async Task<IEnumerable<MedicineIndexViewModel>> GetIndex(string userId)
+        {
+            var Medicines = await _context.Medicines
+                .Where(m => m.IsDeleted == false)
+                .Select(m => new MedicineIndexViewModel
+                {
+                    Id = m.Id,
+                    Name = m.MedicineName,
+                    ExpeationDate = m.ExperationDate,
+                    Price = m.Price,
+                    Description = m.Description,
+                    TypeName = m.MedicineType.MedicineTypeName,
+                    ImageURL = m.ImageURL,
+                    IsPublisher = m.UserId == userId,
+                    HasBought = userId != null && _context.UsersMedicines.Any(um => um.MedicineId == m.Id && um.UserId == userId)
+                })
+                .ToListAsync();
 
+            return Medicines;
+        }
     }
 }
