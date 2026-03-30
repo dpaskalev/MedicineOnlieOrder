b8f399aaf51219bcf6fd3b10945a628963634cd2
Daniel <danielpaskalev14@Gmail.com>
2026-03-30 17:14:50 +0300

    Create "Cart View model"

---
+++ Services/VewModels/CartViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.VewModels
+{
+    public class CartViewModel
+    {
+        [Required]
+        public int MedicineId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public DateTime ExperationDate { get; set; }
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string TypeName { get; set; } = null!;
+
+        public string? ImageURL { get; set; }
+    }
+}
