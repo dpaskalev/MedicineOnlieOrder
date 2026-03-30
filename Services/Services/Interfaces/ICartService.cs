38ded919fa5ed17eed44da9eaa5206ff7d01cf09
Daniel <danielpaskalev14@Gmail.com>
2026-03-30 17:14:22 +0300

    Create cart interface

---
+++ Services/Services/Interfaces/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using Services.VewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services.Interfaces
+{
+    public interface ICartService
+    {
+        public Task<IEnumerable<CartViewModel>> GetIndexAsync(string userId);
+
+        public Task AddAsync(int medicineId, string userId);
+
+        public Task RemoveAsync(int medicineId, string userId);
+    }
+}
