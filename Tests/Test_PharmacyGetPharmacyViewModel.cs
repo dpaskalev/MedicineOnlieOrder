ccdb107b649e61c3da49d7c2971327e894de3b3f
Daniel <danielpaskalev14@Gmail.com>
2026-04-05 12:44:12 +0300

    Crete test for "PharmacyService" "GetPharmacyViewModel"

---
+++ Tests/Test_PharmacyGetPharmacyViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DataModels.Data.DataModels;
+using DataModels.Data;
+using Services.Services.Interfaces;
+using Services.Services;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class Test_PharmacyGetPharmacyViewModel
+    {
+        private ApplicationDbContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB_Medicine_GetAddModel")
+                .Options;
+            this.context = new ApplicationDbContext(options);
+
+
+        }
+
+        [Test]
+        public void Test_GetPharmacyViewModel()
+        {
+            IPharmacyService service = new PharmacyService(this.context);
+
+            var result = service.GetPharmacyViewModel();
+
+            Assert.True(result != null);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            context.Dispose();
+        }
+    }
+}
